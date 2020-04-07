8804ee805b540c9a841e504fcce7747fb37d1255
Filip Glavica <filip.glavica@factory.hr>
2020-04-07 12:47:01 +0200

    Initial Commit

---
+++ SeanTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeanTableView
+//
+//  Created by Filip Glavica on 07/04/2020.
+//  Copyright © 2020 Filip Glavica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
